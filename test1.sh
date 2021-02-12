c7a9df2f793007290149e1944cdc8ffe15fe9041
Simpy Parveen <simpyparveen@gmail.com>
2021-02-12 11:56:28 -0700

    Update and rename Scripts-Examples to test1.sh

---
+++ test1.sh
@@ -0,0 +1,14 @@
+for f in cf*.txt
+do
+	#echo "File name is $f"
+	while IFS= read -r line
+	do
+		if [[ "$line" == "Just printing substring:"* ]]; then
+			echo "$line" | awk '{ print $11 }'
+		fi
+	done < $f
+done
+# bash test1.sh
+# Reading files to recover substring from all files
+
+
