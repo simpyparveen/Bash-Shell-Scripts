59fd62b07506f1a83c989097ed26dfe958c05047
Simpy Parveen <simpyparveen@gmail.com>
2021-02-12 11:57:50 -0700

    Create test3.sh

---
+++ test3.sh
@@ -0,0 +1,15 @@
+for f in cf*.txt
+do
+	#echo "File name is $f"
+	while IFS= read -r line
+	do
+		if [[ "$line" == "Handshake complete with role :DTLS_CLIENT and hs_rtt:"* ]]; then
+			echo "$line" | awk '{ print $11 }'
+		fi
+	done < $f
+done
+
+# bash test3.sh
+# Reading files to recover only hs_rtt from all files
+
+
