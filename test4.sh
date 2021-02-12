14a1f39b662595cc17f3854a97f68e8ee1cb9c85
Simpy Parveen <simpyparveen@gmail.com>
2021-02-12 11:58:57 -0700

    Create test4.sh

---
+++ test4.sh
@@ -0,0 +1,9 @@
+for i in $(seq 1 50)
+  do 
+     echo "Welcome $i times"
+	./dtls-client 136.159.7.172 -i identity.txt -k pskkey.txt > cf$i.txt
+ done
+
+
+# RUN : sh test4.sh
+# to run client multiple time consecutively
